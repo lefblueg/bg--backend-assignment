88a43cd77cc2922cc404dd7759e883a5b5c73190
Lampros Eleftherios <elampros@qualco.eu>
2022-01-09 23:15:52 +0200

    release 1.0

--- bank-account/bankacc.core.api/src/main/java/com/marscolony/bankacc/core/api/models/AccountType.java
+++
@@ -1,5 +0,0 @@
-package com.marscolony.bankacc.core.api.models;
-
-public enum AccountType {
-    SAVINGS, CURRENT
-}
--- bank-account/bankacc.query.api/src/main/java/com/marscolony/bankacc/query/api/repositories/AccountRepository.java
+++
@@ -1,7 +0,0 @@
-package com.marscolony.bankacc.query.api.repositories;
-
-import com.marscolony.bankacc.core.api.models.BankAccount;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AccountRepository extends CrudRepository<BankAccount, String> {
-}
